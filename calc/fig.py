d648f703d11c98cc323ad172a7fe6f1beaba4742
sksmsfbrjs <sksmsfbrjs@naver.com>
2020-06-20 17:20:33 +0900

    Issue #5 when I start apache2, change(sum : , product : 0) into (sum : 0, product : 0)

--- calc/fig.py
+++ calc/fig.py
@@ -5,13 +5,14 @@ def application(environ, start_response):
 	d=parse_qs(environ['QUERY_STRING'])
 	a = d.get('a', [''])[0]
 	b = d.get('b', [''])[0]
+	x = 0
 	y = 0
 	if '' not in [a,b]:
 	    a, b = int(a), int(b)
-	    global y
-	    y = str(a*b)
+	    x = a + b
+	    y = a * b
 	response_body = html % {
-	"sum" : a + b,
+	"sum" : x,
 	"product" : y 
 	}
 	start_response('200 OK', [
